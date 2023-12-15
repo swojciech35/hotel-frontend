474c9a39d76c614aee1d16ab887b75e82afea9a8
Schabiik <swojciech35@gmail.com>
2023-12-15 10:19:07 +0100

    modal

--- src/element/elements/HotelRoomCard.tsx
+++ src/element/elements/HotelRoomCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {Image} from "react-bootstrap";
 import {LuBedSingle} from "react-icons/lu";
 import {MdPerson} from "react-icons/md";
@@ -7,11 +7,13 @@ import {FaCheck} from "react-icons/fa";
 import {FaXmark} from "react-icons/fa6";
 import {MdOutlineBalcony} from "react-icons/md";
 import {CustomButton} from "./CustomButton";
+import {ReservationModalElement} from "./ReservationModalElement";
 
 export const HotelRoomCard = (props: HotelRoomCardType) => {
 
+    const [showModal,setShowModal]=useState(false)
     return (<>
-        <div className={'d-flex flex-row rounded-4 border border-black border-4'}>
+        <div className={'d-flex flex-row rounded-4 border border-black border-4 m-4'}>
             <div className={"d-flex flex-column p-3"} style={{width: '50%'}}>
                 <Image className="img-fluid rounded-4 border border-2 border-black"
                        src={props.photoUrl}
@@ -26,7 +28,7 @@ export const HotelRoomCard = (props: HotelRoomCardType) => {
                         <strong>{props.numberOfBeds}</strong></div>
                     <div className={'d-flex align-items-center'}>
                         <MdPerson size={30} className={'me-2'}/>
-                        <strong>{props.numberOfBeds}</strong></div>
+                        <strong>{props.numberOfPeople}</strong></div>
                 </div>
                 <div className={'d-flex flex-row justify-content-evenly mx-5 my-2'} style={{width: '100%'}}>
                     <div className={'d-flex align-items-center'}>
@@ -41,9 +43,11 @@ export const HotelRoomCard = (props: HotelRoomCardType) => {
                      style={{width: '100%'}}>
                     <h4>Cena: {props.pricePerDay} zł/dzień</h4>
                     <div className={'pb-2'}>
-                        <CustomButton value={"Zarezerwuj"}/>
+                        <CustomButton value={"Zarezerwuj"} onClick={()=> {
+                            setShowModal(true)
+                        }}/>
                     </div>
-
+                    <ReservationModalElement show={showModal} onClose={()=>setShowModal(false)} name={props.name} numberOfBeds={props.numberOfBeds} numberOfPeople={props.numberOfPeople} pricePerDay={props.pricePerDay}/>
                 </div>
             </div>
 
